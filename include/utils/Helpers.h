75820186387093b3e741a14ee3bc7df4400e5305
Minh Hien <pmh23112005@gmail.com>
2025-09-27 08:21:09 +0700

    .

--- include/utils/Helpers.h
+++ include/utils/Helpers.h
@@ -9,11 +9,10 @@ public:
     static std::string hashPassword(const std::string& password);
     static std::vector<std::string> splitString(const std::string& str, char delimiter);
     static std::string generatePNR();
-    static std::string getCurrentDateTime();
+    // static std::string getCurrentDateTime();
     static double calculateTicketPrice(double basePrice, const std::string& seatClass, 
                                        int passengerAge, bool hasPromotion);
     static std::string formatCurrency(double amount);
-    static bool isValidEmail(const std::string& email);
     static bool isValidPhoneNumber(const std::string& phone);
     static std::string generateRandomString(int length);
 };
