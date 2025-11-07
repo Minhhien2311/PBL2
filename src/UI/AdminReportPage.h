8c39fd8290e7442540f9c72827bc1dd18f66bfda
Minh Hien <pmh23112005@gmail.com>
2025-11-08 00:32:20 +0700

    ...

--- src/UI/AdminReportPage.h
+++ src/UI/AdminReportPage.h
@@ -1,36 +1,67 @@
 #pragma once
 
 #include <QWidget>
-#include <QTableWidget>
-#include <QPushButton>
-#include <QDateEdit>
+#include <QDate>
+#include <QFrame>
 #include <QLabel>
+#include <QListWidget>
+#include <QStackedWidget>
+#include <QDateEdit>
+#include <QPushButton>
 
+// Forward declarations
 class AccountManager;
 class BookingManager;
 class ReportManager;
 
-class AdminReportPage : public QWidget {
+// Thay đổi: Không dùng QtCharts namespace, include trực tiếp
+#include <QChartView>
+#include <QChart>
+#include <QBarSeries>
+
+class AdminReportPage : public QWidget
+{
     Q_OBJECT
+
 public:
     explicit AdminReportPage(AccountManager* am,
                              BookingManager* bm,
                              ReportManager* rm,
                              QWidget* parent = nullptr);
+    ~AdminReportPage();
+
+private slots:
+    void onMenuSelectionChanged(int index);
+    void onTicketsReportRefresh();
 
 private:
+    void setupUI();
+    QWidget* createLeftMenu();
+    QWidget* createRightPane();
+    
+    QWidget* createTicketsSoldPage();
+    QWidget* createRevenuePage();
+
+    QFrame* createSummaryBox(const QString& title, QLabel*& valueLabel);
+    void updateChart(const std::string& start, const std::string& end);
+
     AccountManager* accountManager_;
     BookingManager* bookingManager_;
     ReportManager* reportManager_;
 
-    QTableWidget* table_;
-    QDateEdit* startDate_;
-    QDateEdit* endDate_;
-    QPushButton* refreshBtn_;
+    QListWidget* menuList_;
+    QStackedWidget* pagesStack_;
 
-    void setupUI();
-    void loadReport();
-
-private slots:
-    void onRefreshClicked();
+    // --- Thành phần của trang "Vé đã bán" ---
+    QDateEdit* ticketsStartDate_;
+    QDateEdit* ticketsEndDate_;
+    QPushButton* ticketsRefreshBtn_;
+    QLabel* totalTicketsLabel_;
+    QLabel* businessTicketsLabel_;
+    QLabel* economyTicketsLabel_;
+    
+    // Thành phần biểu đồ - KHÔNG dùng QtCharts::
+    QChartView* chartView_;
+    QChart* chart_;
+    QBarSeries* chartSeries_;
 };
